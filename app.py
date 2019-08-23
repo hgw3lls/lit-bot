93ef7108776bf12ad5548f4829dc5d3019b11749
Quang Nguyen <quang.p.nguyen.gr@dartmouth.edu>
2019-08-23 17:47:08 -0400

    Final fixing for url requests

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=["GET"])
+def hello():
+    return("Hello World")
+
+@app.route('/verification', methods = ["POST"])
 def getting_challenge():
-    slack_req = json.loads(request.form.get('payload'))
-    response = {"challenge": slack_req['challenge']}
+    slack_req = request.get_json()['challenge']
+    response = {'challenge':slack_req}
     return(response)
+
 
 if __name__ == "__main__":
     app.run()
